70bb24947d3a0cd88f1f46b533e7c49a6646040c
Gabby <GabbyVByers@gmail.com>
2025-09-21 19:26:54 -0700

    mixing things up and seeing if i like snake case and/or inline brackets

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main() {
+    std::ifstream source_file("snake.vc");
+    std::string content((std::istreambuf_iterator<char>(source_file)), std::istreambuf_iterator<char>());
+
+    std::string temp_content = content;
+    content.clear();
+    for (char& ch : temp_content) {
+        if (ch == '\n')
+            continue;
+        if (ch == '\t')
+            continue;
+        content.push_back(ch);
+    }
+
+    std::vector<std::string> lines;
+    std::string current_line;
 
 
-
-
-
-int main()
-{
-	return 0;
+    std::cout << content;
+    return 0;
 }
